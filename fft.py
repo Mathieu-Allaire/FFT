3e637f20dfee27a151b0686faed5fdfba337ca9d
Jatin Patel <jatin_pat@outlook.com>
2024-11-29 17:19:39 -0500

    denoise and compression

--- fft.py
+++ fft.py
@@ -7,13 +7,16 @@ import argparse
 class DiscreteFourierTransform:
     def __init__(self, image):
         self.original_image = image
-        self.resized_image = self.resize_image(image)
+        self.resized_image = self.pad_image(image)
         
-    def resize_image(self, image):
+    def pad_image(self, image):
         row, col = image.shape
-        resized_row, resized_col = 2**int(np.ceil(np.log2(row))), 2**int(np.ceil(np.log2(col)))
         
-        padded_image = np.zeros((resized_row, resized_col), dtype=np.uint8)
+        # Pad the image to the next power of 2
+        next_power_of_2 = lambda x: 2**int(np.ceil(np.log2(x)))
+        resized_row, resized_col = next_power_of_2(row), next_power_of_2(col)
+        
+        padded_image = np.zeros((resized_row, resized_col))
         padded_image[:row, :col] = image
         
         return padded_image 
@@ -92,45 +95,69 @@ class DiscreteFourierTransform:
         
         return col_transformed_image
     
-    def plot_compression(self):
-        return 0
+    def plot_fft(self):
+        frequency_domain = self.fft_2d()
+        
+        fig, axs = plt.subplots(1, 2)
+        axs[0].imshow(self.original_image, cmap='gray')
+        axs[0].set_title("Original Image")
+
+        axs[1].imshow(np.abs(frequency_domain)[:self.original_image.shape[0], :self.original_image.shape[1]], norm=LogNorm())
+        axs[1].set_title("Fourier Transform")
+        
+        plt.show()
     
-    def plot_denoise(self):
-        frequency_domaine = self.fft_2d()
-
-        magnitude = np.abs(frequency_domaine)
-        frequency_domaine[magnitude > np.quantile(magnitude, 1 - 0.00099)] = 0
-
-        denoised_image = self.ifft_2d(frequency_domaine)
+    def plot_denoise(self, low_frequency_threshold=0.1):
+        frequency_domain = self.fft_2d()
+        row, col = frequency_domain.shape
+        
+        low_frq_row_start, low_frq_row_end = int(row*low_frequency_threshold), int(row*(1-low_frequency_threshold))
+        low_frq_col_start, low_frq_col_end = int(col*low_frequency_threshold), int(col*(1-low_frequency_threshold))
+        
+        frequency_domain[low_frq_row_start:low_frq_row_end, :] = 0
+        frequency_domain[:, low_frq_col_start:low_frq_col_end] = 0
+        
+        denoised_image = self.ifft_2d(frequency_domain)
         denoised_image = np.real(denoised_image)
         denoised_image = denoised_image[:self.original_image.shape[0], :self.original_image.shape[1]]
-
-        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+        
+        fig, axs = plt.subplots(1, 2)
         axs[0].imshow(self.original_image, cmap='gray')
         axs[0].set_title("Original Image")
-
+        
         axs[1].imshow(denoised_image, cmap='gray')
-        axs[1].set_title("FFT Denoised Image")
-
-        plt.show()
-    
-    def plot_fft(self):
-        frequency_domaine = self.fft_2d()
-        magnitude = np.abs(frequency_domaine)
-        
-        fft_image = magnitude[:self.original_image.shape[0], :self.original_image.shape[1]]
-
-
-        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-        axs[0].imshow(self.original_image, cmap='gray')
-        axs[0].set_title("Original Image")
-
-        # Custom Fourier Transform (log scaled)
-        axs[1].imshow(fft_image, norm=LogNorm(), cmap='gray')
-        axs[1].set_title("FFT Image")
+        axs[1].set_title("Denoised Image")
         
         plt.show()
         
+    def plot_compression(self):
+        frequency_domain = self.fft_2d()
+        magnitude = np.abs(frequency_domain)
+        row, col = frequency_domain.shape
+        compression_levels = [0, 50, 90, 95, 99, 99.9]
+        
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+        axs = axs.flatten()
+        
+        total_coefficients = row * col
+        
+        for i, compression_level in enumerate(compression_levels):
+            print(f'Compression level: {compression_level}%')
+            
+            threshold = np.percentile(magnitude, compression_level)
+            mask = magnitude >= threshold
+            frequency_domain_compressed = frequency_domain * mask
+            
+            compressed_image = self.ifft_2d(frequency_domain_compressed)
+            compressed_image = np.real(compressed_image)
+            compressed_image = compressed_image[:self.original_image.shape[0], :self.original_image.shape[1]]
+
+            axs[i].imshow(compressed_image, cmap='gray')
+            axs[i].set_title(f'{compression_level}% Compression')
+        
+        plt.tight_layout()
+        plt.show()
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', type=int, required=False, help='Mode: 1 for Fast mode, 2 for Denoise, 3 for Compress, 4 for Plot runtime', default=1)
@@ -147,7 +174,7 @@ def main():
     
     # Perform requested mode
     if mode == 1:
-        dft.display_fft()
+        dft.plot_fft()
     elif mode == 2:
         dft.plot_denoise()
     elif mode == 3:

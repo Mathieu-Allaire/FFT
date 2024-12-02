488786ed99bd6fdf5adde1289b18eafd6d2748a9
Jatin Patel <jatin_pat@outlook.com>
2024-12-02 15:05:17 -0500

    finalized code, missing plot runtime

--- fft.py
+++ fft.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import cv2
+import os
+import time
 import argparse
 
 class DiscreteFourierTransform:
     def __init__(self, image):
         self.original_image = image
         self.resized_image = self.pad_image(image)
-        
+    
     def pad_image(self, image):
         row, col = image.shape
         
@@ -46,36 +48,76 @@ class DiscreteFourierTransform:
     def fft(self, signal, size_threshold=8):
         N = len(signal)
         
+        # If the signal is small enough, use DFT
         if N <= size_threshold:
             return self.dft(signal)
         
+        # Split the signal into even and odd parts (divide and conquer)
         even = self.fft(signal[0::2])
         odd = self.fft(signal[1::2])
         
+        # Precompute the coefficients
         coeff = np.exp(-2j * np.pi * np.arange(N) / N)
         
+        # Combine the results using symmetry properties of the DFT
         return np.concatenate([even + coeff[:N//2] * odd, even + coeff[N//2:] * odd])  
     
     def ifft(self, signal, size_threshold=8):
         N = len(signal)
         
+        # If the signal is small enough, use IDFT
         if N <= size_threshold:
             return self.idft(signal)
         
+        # Split the signal into even and odd parts (divide and conquer)
         even = self.ifft(signal[0::2])
         odd = self.ifft(signal[1::2])
         
+        # Precompute the coefficients
         coeff = np.exp(2j * np.pi * np.arange(N) / N)
         
+        # Combine the results using symmetry properties of the IDFT
         return np.concatenate([even + coeff[:N//2] * odd, even + coeff[N//2:] * odd])
+    
+    def dft_2d(self):
+        row, col = self.resized_image.shape
+        
+        # Perform 1D DFT on rows
+        row_transformed_image = np.zeros((row, col), dtype=np.complex128)
+        for i in range(row):
+            row_transformed_image[i, :] = self.dft(self.resized_image[i, :])
+        
+        # Perform 1D DFT on columns of the transformed image
+        col_transformed_image = np.zeros((row, col), dtype=np.complex128)
+        for j in range(col):
+            col_transformed_image[:, j] = self.dft(row_transformed_image[:, j])
+            
+        return col_transformed_image
+    
+    def idft_2d(self, transformed_image):
+        row, col = transformed_image.shape
+        
+        # Perform 1D IDFT on rows
+        row_transformed_image = np.zeros((row, col), dtype=np.complex128)
+        for i in range(row):
+            row_transformed_image[i, :] = self.idft(transformed_image[i, :])
+        
+        # Perform 1D IDFT on columns of the transformed image
+        col_transformed_image = np.zeros((row, col), dtype=np.complex128)
+        for j in range(col):
+            col_transformed_image[:, j] = self.idft(row_transformed_image[:, j])
+        
+        return col_transformed_image
     
     def fft_2d(self):
         row, col = self.resized_image.shape
         
+        # Perform 1D FFT on rows
         row_transformed_image = np.zeros((row, col), dtype=np.complex128)
         for i in range(row):
             row_transformed_image[i, :] = self.fft(self.resized_image[i, :])
         
+        # Perform 1D FFT on columns of the transformed image
         col_transformed_image = np.zeros((row, col), dtype=np.complex128)
         for j in range(col):
             col_transformed_image[:, j] = self.fft(row_transformed_image[:, j])
@@ -85,10 +127,12 @@ class DiscreteFourierTransform:
     def ifft_2d(self, transformed_image):
         row, col = transformed_image.shape
         
+        # Perform 1D IFFT on rows
         row_transformed_image = np.zeros((row, col), dtype=np.complex128)
         for i in range(row):
             row_transformed_image[i, :] = self.ifft(transformed_image[i, :])
         
+        # Perform 1D IFFT on columns of the transformed image
         col_transformed_image = np.zeros((row, col), dtype=np.complex128)
         for j in range(col):
             col_transformed_image[:, j] = self.ifft(row_transformed_image[:, j])
@@ -102,6 +146,7 @@ class DiscreteFourierTransform:
         axs[0].imshow(self.original_image, cmap='gray')
         axs[0].set_title("Original Image")
 
+        # Plot the Fourier Transform; take the absolute value to get the magnitude; log-scaled
         axs[1].imshow(np.abs(frequency_domain), norm=LogNorm())
         axs[1].set_title("Fourier Transform")
         
@@ -111,9 +156,12 @@ class DiscreteFourierTransform:
         frequency_domain = self.fft_2d()
         row, col = frequency_domain.shape
         
+        # determine boundaries for the low frequency components
         low_frq_row_start, low_frq_row_end = int(row*low_frequency_threshold), int(row*(1-low_frequency_threshold))
         low_frq_col_start, low_frq_col_end = int(col*low_frequency_threshold), int(col*(1-low_frequency_threshold))
         
+        # set the high frequency components to zero, representing a cross-like shape in the frequency domain
+        # We are left with the low frequency components in the corners of the frequency domain
         frequency_domain[low_frq_row_start:low_frq_row_end, :] = 0
         frequency_domain[:, low_frq_col_start:low_frq_col_end] = 0
         
@@ -122,8 +170,10 @@ class DiscreteFourierTransform:
         print(f'Non-zero coefficients: {non_zero_coefficients}')
         print(f'Fraction of non-zero coefficients: {non_zero_coefficients/total_coefficients}')
         
+        # Perform the inverse Fourier Transform to get the denoised image
         denoised_image = self.ifft_2d(frequency_domain)
         denoised_image = np.real(denoised_image)
+        # Crop the image to the original size for beter comparison
         denoised_image = denoised_image[:self.original_image.shape[0], :self.original_image.shape[1]]
         
         fig, axs = plt.subplots(1, 2)
@@ -139,9 +189,12 @@ class DiscreteFourierTransform:
         frequency_domain = self.fft_2d()
         row, col = frequency_domain.shape
         
+        # determine boundaries for the high frequency components
         high_frq_row_start, high_frq_row_end = int(row*high_frequency_threshold), int(row*(1-high_frequency_threshold))
         high_frq_col_start, high_frq_col_end = int(col*high_frequency_threshold), int(col*(1-high_frequency_threshold))
         
+        # set the low frequency components to zero, representing the corners of the frequency domain
+        # We are left with the high frequency components representing a cross-like shape in the frequency domain
         frequency_domain[:high_frq_row_start, :high_frq_col_start] = 0
         frequency_domain[:high_frq_row_start, high_frq_col_end:] = 0
         frequency_domain[high_frq_row_end:, :high_frq_col_start] = 0
@@ -152,6 +205,7 @@ class DiscreteFourierTransform:
         print(f'Non-zero coefficients: {non_zero_coefficients}')
         print(f'Fraction of non-zero coefficients: {non_zero_coefficients/total_coefficients}')
         
+        # Perform the inverse Fourier Transform to get the denoised image
         denoised_image = self.ifft_2d(frequency_domain)
         denoised_image = np.real(denoised_image)
         denoised_image = denoised_image[:self.original_image.shape[0], :self.original_image.shape[1]]
@@ -169,6 +223,7 @@ class DiscreteFourierTransform:
         frequency_domain = self.fft_2d()
         row, col = frequency_domain.shape
 
+        # Thresholding the magnitude of the frequency domain
         magnitude = np.abs(frequency_domain)
         threshold = np.quantile(magnitude, 1 - threshold)
         frequency_domain[magnitude > threshold] = 0
@@ -178,6 +233,7 @@ class DiscreteFourierTransform:
         print(f'Non-zero coefficients: {non_zero_coefficients}')
         print(f'Fraction of non-zero coefficients: {non_zero_coefficients/total_coefficients}')
 
+        # Perform the inverse Fourier Transform to get the denoised image
         denoised_image = self.ifft_2d(frequency_domain)
         denoised_image = np.real(denoised_image)
         denoised_image = denoised_image[:self.original_image.shape[0], :self.original_image.shape[1]]
@@ -202,23 +258,120 @@ class DiscreteFourierTransform:
         
         total_coefficients = row * col
         
+        # Compress the image for different compression levels
         for i, compression_level in enumerate(compression_levels):
             print(f'Compression level: {compression_level}%')
             
+            # Threshold the magnitude of the frequency domain
             threshold = np.percentile(magnitude, compression_level)
             mask = magnitude >= threshold
+            # Multiply the frequency domain with the mask to set the high frequency components to zero
             frequency_domain_compressed = frequency_domain * mask
             
+            non_zero_coefficients = np.count_nonzero(frequency_domain)
+            total_coefficients = row * col
+            print(f'Non-zero coefficients: {non_zero_coefficients}')
+            print(f'Fraction of non-zero coefficients: {non_zero_coefficients/total_coefficients}')
+            
+            # Perform the inverse Fourier Transform to get the compressed image
             compressed_image = self.ifft_2d(frequency_domain_compressed)
             compressed_image = np.real(compressed_image)
             compressed_image = compressed_image[:self.original_image.shape[0], :self.original_image.shape[1]]
+            
+            # Save the compressed image to determine the file size
+            file_name = f'compressed_image_{compression_level}.png'
+            cv2.imwrite(file_name, compressed_image)
+            file_size = os.path.getsize(file_name)
+            print(f"Size of {file_name}: {file_size} bytes")
 
             axs[i].imshow(compressed_image, cmap='gray')
             axs[i].set_title(f'{compression_level}% Compression')
         
         plt.tight_layout()
         plt.show()
+    
+    def plot_compression_alternative(self):
+        frequency_domain = self.fft_2d()
+        magnitude = np.abs(frequency_domain)
+        row, col = frequency_domain.shape
+        compression_levels = [0, 50, 90, 95, 99, 99.9]
 
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+        axs = axs.flatten()
+
+        total_coefficients = row * col
+
+        # Compress the image for different compression levels
+        for i, compression_level in enumerate(compression_levels):
+            print(f'Compression level: {compression_level}%')
+
+            # Threshold the magnitude of the frequency domain
+            high_threshold = np.percentile(magnitude, compression_level)
+            high_mask = magnitude >= high_threshold
+
+            low_frequency_threshold = 0.05  # Define a threshold for "low frequencies"
+            
+            # Determine the boundaries for the low frequency components
+            low_frq_row_start, low_frq_row_end = int(row * low_frequency_threshold), int(row * (1 - low_frequency_threshold))
+            low_frq_col_start, low_frq_col_end = int(col * low_frequency_threshold), int(col * (1 - low_frequency_threshold))
+            low_mask = np.zeros_like(magnitude, dtype=bool)
+            low_mask[low_frq_row_start:low_frq_row_end, low_frq_col_start:low_frq_col_end] = True
+
+            # Combine the high and low frequency masks
+            combined_mask = high_mask | low_mask
+            
+            # Multiply the frequency domain with the mask to set the high frequency components to zero
+            frequency_domain_compressed = frequency_domain * combined_mask
+            
+            non_zero_coefficients = np.count_nonzero(frequency_domain)
+            total_coefficients = row * col
+            print(f'Non-zero coefficients: {non_zero_coefficients}')
+            print(f'Fraction of non-zero coefficients: {non_zero_coefficients/total_coefficients}')
+
+            # Perform the inverse Fourier Transform to get the compressed image
+            compressed_image = self.ifft_2d(frequency_domain_compressed)
+            compressed_image = np.real(compressed_image)
+            compressed_image = compressed_image[:self.original_image.shape[0], :self.original_image.shape[1]]
+
+            # Save the compressed image to determine the file size
+            file_name = f'compressed_image_{compression_level}.png'
+            cv2.imwrite(file_name, compressed_image)
+            file_size = os.path.getsize(file_name)
+            print(f"Size of {file_name}: {file_size} bytes")
+
+            axs[i].imshow(compressed_image, cmap='gray')
+            axs[i].set_title(f'{compression_level}% Compression')
+
+        plt.tight_layout()
+        plt.show()
+        
+    def plot_runtime(self):
+        return 0
+    
+    def validate_fft(self):
+        # Custom FFT
+        frequency_domain = self.fft_2d()
+        # Numpy FFT
+        frequency_domain_numpy = np.fft.fft2(self.resized_image)
+        
+        # Compare the results
+        fig, axs = plt.subplots(1, 3)
+
+        # Plot custom FFT log-scaled
+        axs[0].imshow(np.abs(frequency_domain), norm = LogNorm())
+        axs[0].set_title("Custom FFT")
+
+        # Plot built-in FFT log-scaled
+        axs[1].imshow(np.abs(frequency_domain_numpy), norm = LogNorm())
+        axs[1].set_title("Built-in FFT")
+        
+        # Plot the difference
+        difference = frequency_domain - frequency_domain_numpy
+        axs[2].imshow(np.abs(difference), cmap='hot')
+        axs[2].set_title("Difference")
+
+        plt.show()
+        
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', type=int, required=False, help='Mode: 1 for Fast mode, 2 for Denoise, 3 for Compress, 4 for Plot runtime', default=1)
@@ -246,8 +399,12 @@ def main():
         dft.plot_denoise_low_frequency()
     elif mode == 6:
         dft.plot_denoise_thresholding()
+    elif mode == 7:
+        dft.plot_compression_alternative()
+    elif mode == 8:
+        dft.validate_fft()
     else:
-        print('Invalid mode. Mode: 1 for Fast mode, 2 for Denoise, 3 for Compress, 4 for Plot runtime')
+        print('Invalid mode. Mode: 1 for Fast mode, 2 for High Frequency Denoise, 3 for Compress, 4 for Plot runtime, 5 for Low Frequency Denoise, 6 for Thresholding Denoise, 7 for Alternative Compression, 8 for Fast mode validation')
         exit()
     
 if __name__ == '__main__':
